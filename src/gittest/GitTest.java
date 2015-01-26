b69005d66620081e24999aebe2358fd4278602c8
Owner <Owner@David-Arnell.wi.rr.com>
2015-01-26 08:28:58 -0600

    GitHub Test 1-26-2015

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,14 @@
+//Just testing out pushing to GitHub
+package gittest;
+import java.util.*;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        String helloWorld = "Hello World";
+        
+        System.out.println(helloWorld);
+        
+    }
+    
+}
